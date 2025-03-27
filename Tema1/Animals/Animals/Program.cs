75cffde9826f83a60804811fb70d856a8dff1813
beatrice0604 <beatricesch04@gmail.com>
2025-03-27 17:45:20 +0200

    Implementare finala pentru problema Animal sounds
    - Creare structura pentru rezolvarea problemei
    - Instantiere 3 obiecte de tip diferit (fiecare clasa derivata - Cat, Otter, Rooster) mosteneste clasa de baza Animal
    - Apelul metodei care afiseaza un mesaj corespunzator in functie de obiect

---
+++ Tema1/Animals/Animals/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Animals;
+using Animals.Animals;
+using System;
+
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Rooster rooster = new Rooster();    
+        Cat cat = new Cat();    
+        Otter otter = new Otter();  
+
+        //var animals = new List<Animal>();
+        //animals.Add(rooster);   
+        //animals.Add(cat);   
+        //animals.Add(otter);  
+
+        var animalsI  = new List<IAnimal>();
+        animalsI.Add(rooster);
+        animalsI.Add(cat);
+        animalsI.Add(otter);
+
+        foreach (var animal in animalsI)
+        {
+            animal.makeSound();
+        }
+    }
+}
