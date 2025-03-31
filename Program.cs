f2106cb794404e5ddb75fb26935841274b913482
Schuller Beatrice <beatricesch04@gmail.com>
2025-03-31 09:55:28 +0300

    Code improvements based on review feedback
    Refactored MakeSound to use dynamic class names
    Removed unnecessary abstract class Animal
    Refactored animal list initialization

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Animals;
+using Animals.Animals;
+using System;
+
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        var animals = new List<IAnimal>()
+        {
+            new Rooster(),
+            new Cat(),
+            new Otter()
+        };
+
+        foreach (var animal in animals)
+        {
+            animal.MakeSound();
+        }
+    }
+}
